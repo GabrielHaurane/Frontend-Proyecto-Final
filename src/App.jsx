51ce07a68b5e8d7b0269bcfbcb50b14e3416c848
Gabriel Haurane <gabrielhaurane@gmail.com>
2024-10-18 16:53:14 -0300

    Agregado de los archivos faltantes e importacion hacia app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
 import './App.css'
-
+import Menu from './components/common/Menu.jsx'
+import Footer from './components/common/Footer.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import DetalleHabitacion from './components/pages/DetalleHabitacion.jsx'
+import Administrador from './components/pages/Administrador.jsx'
+import Login from './components/pages/Login.jsx'
+import Inicio from './components/pages/Inicio.jsx'
+import Ajustes from './components/pages/Ajustes.jsx'
+import Catalogo from './components/pages/Catalogo.jsx'
+import Contacto from './components/pages/Contacto.jsx'
+import Galeria from './components/pages/Galeria.jsx'
+import Quienes from './components/pages/Quienes.jsx'
+import Error404 from './components/pages/Error404.jsx'
+import RutasAdmin from './components/pages/RutasAdmin.jsx'
+import RutasProtegidas from './components/pages/RutasProtegidas.jsx'
 function App() {
  
 
