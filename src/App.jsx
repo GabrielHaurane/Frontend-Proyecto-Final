53bbf3e7a5a81ee98cd398eec23de89904a9534e
Gabriel Haurane <gabrielhaurane@gmail.com>
2024-10-18 17:12:24 -0300

    agregue el apartado del browserRouter y agregue contenido a la RutasProtegidas.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,34 @@ import Contacto from './components/pages/Contacto.jsx'
 import Galeria from './components/pages/Galeria.jsx'
 import Quienes from './components/pages/Quienes.jsx'
 import Error404 from './components/pages/Error404.jsx'
-import RutasAdmin from './components/pages/RutasAdmin.jsx'
-import RutasProtegidas from './components/pages/RutasProtegidas.jsx'
+import RutasAdmin from './components/routes/RutasAdmin.jsx'
+import RutasProtegidas from './components/routes/RutasProtegidas.jsx'
+import { useState } from 'react'
 function App() {
- 
+  const usuario = JSON.parse(sessionStorage.getItem('userKey')) || '';
+  const [usuarioLogueado, setUsuarioLogeado] = useState(usuario)
 
   return (
     <>
-      
+      <BrowserRouter>
+      <Menu></Menu>
+      <Routes>
+        <Route exact path='/' element={<Inicio></Inicio>} ></Route>
+        <Route exact path='/quienessomos'element={<Quienes></Quienes>}></Route>
+        <Route exact path='/contacto' element={<Contacto></Contacto>}></Route>
+        <Route exact path='/galeria' element={<Galeria></Galeria>}></Route>
+        <Route exact path='/catalogo' element={<Catalogo></Catalogo>}></Route>
+        <Route exact path='/detallehabitacion/:id' element={<DetalleHabitacion></DetalleHabitacion>}></Route>
+        <Route exact path='/login' element={<Login setUsuarioLogeado={setUsuarioLogeado}></Login>}></Route>
+        <Route exact path='/administrador/*' element={
+          <RutasProtegidas>
+            <RutasAdmin></RutasAdmin>
+          </RutasProtegidas>
+        }></Route>
+        <Route exact path='/ajustes' element={<Ajustes></Ajustes>}></Route>
+        <Route exact path='*' element={<Error404></Error404>}></Route>
+      </Routes>
+      </BrowserRouter>
     </>
   )
 }
