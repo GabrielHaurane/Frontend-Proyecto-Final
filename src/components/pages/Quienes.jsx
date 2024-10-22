1df2cfa1aacc0e5619b2718302c386c6934f3cc6
Gabriel Haurane <gabrielhaurane@gmail.com>
2024-10-22 02:56:54 -0300

    El archivo de quienes somos ya esta casi terminado falta la info de augusto

--- src/components/pages/Quienes.jsx
+++ src/components/pages/Quienes.jsx
@@ -1,11 +1,52 @@
-import React from 'react';
+import React from "react";
+import { Card } from "react-bootstrap";
+import gabrielH from "../assets/Gabriel_Haurane.jpg";
 
 const Quienes = () => {
-    return (
-        <div>
-            
+  return (
+    <div className="flex-grow-1 backQS">
+        <div className="container">
+      <Card className="backQ my-3 d-flex flex-md-row  flex-column border border-2 border-black">
+        <div className="d-flex w-100 justify-content-center align-items-center">
+          <img className="col-12 h-75"  src={gabrielH}/>
         </div>
-    );
+        <Card.Body className="col-12 d-flex flex-row">
+          <div className="d-flex flex-column flex-wrap justify-content-center align-items-center">
+          <Card.Title>Gabriel Alejandro Haurane</Card.Title>
+          <Card.Text>
+            <b>Desarrollador</b>
+          </Card.Text>
+          <Card.Text>
+            “Nuestra misión es brindar una estancia única y acogedora, donde
+            nuestros huéspedes se sientan como en casa, rodeados de comodidad y
+            lujo. Nuestro objetivo es ser el destino preferido para aquellos que
+            buscan calidad, tranquilidad y atención personalizada.”
+          </Card.Text>
+          </div>
+        </Card.Body>
+      </Card>
+      <Card className="backQ my-3 d-flex flex-md-row  flex-column border border-2 border-black">
+        <div className="d-flex w-100 justify-content-center align-items-center">
+          <img className="col-12 h-75"  src={gabrielH}/>
+        </div>
+        <Card.Body className="col-12 d-flex flex-row">
+          <div className="d-flex flex-column flex-wrap justify-content-center align-items-center">
+          <Card.Title>Augusto Brito</Card.Title>
+          <Card.Text>
+            <b>Desarrollador</b>
+          </Card.Text>
+          <Card.Text>
+            “Nuestra misión es brindar una estancia única y acogedora, donde
+            nuestros huéspedes se sientan como en casa, rodeados de comodidad y
+            lujo. Nuestro objetivo es ser el destino preferido para aquellos que
+            buscan calidad, tranquilidad y atención personalizada.”
+          </Card.Text>
+          </div>
+        </Card.Body>
+      </Card>
+        </div>
+    </div>
+  );
 };
 
 export default Quienes;
