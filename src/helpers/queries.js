d63e3fa80dcba411fea07a2ea00b21bbc383c742
Augusto Brito <britoaugusto1@gmail.com>
2024-10-30 15:33:53 -0300

    Se modificaron la queries para obtenerhabitacionesdisponible y tambien la funcion que la llama en el componente Catalogo

--- src/helpers/queries.js
+++ src/helpers/queries.js
@@ -165,15 +165,20 @@ export const obtenerHabitacionAdmin = async (idHabitacion) => {
 
 export const listadoHabitacionesDisponibles = async (req,res) =>{
   try {
-    const respuesta = await fetch(`${URLHabitacion}/disponibles`)
+    const respuesta = await fetch(`${URLHabitacion}/disponibles`,
+      {
+        headers:{
+          "x-token": JSON.parse(sessionStorage.getItem("userKey")).token
+        }
+      }
+    )
     if (!respuesta.ok) {
       throw new Error("Error al obtener los detalles de la habitación");
     }
-    return respuesta;
+    const data = await respuesta.json()
+    return data;
   } catch (error) {
     console.error(error)
-    res.status(500).json({
-      mensaje: "Error al obtener las habitaciones disponibles"
-    })
+    return false;
   }
 }
